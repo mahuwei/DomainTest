08f638eb5f667db347e9f241698d59ba28186db5
mahuwei <mahuwei@msn.com>
2020-04-22 22:21:22 +0800

    基础ocelot应用。

---
+++ GatewayApis/BusinessApi/Controllers/WFController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+
+namespace BusinessApi.Controllers {
+  [ApiController]
+  [Route("business/[controller]")]
+  public class WFController : ControllerBase {
+    private static readonly string[] Summaries = {
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WFController> _logger;
+
+    public WFController(ILogger<WFController> logger) {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get() {
+      _logger.LogInformation($"{HttpContext.Request.GetDisplayUrl()}");
+      var rng = new Random();
+      return Enumerable.Range(1, 5)
+        .Select(index => new WeatherForecast {
+          Date = DateTime.Now.AddDays(index),
+          TemperatureC = rng.Next(-20, 55),
+          Summary = Summaries[rng.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet]
+    [Route("top")]
+    public IEnumerable<WeatherForecast> Get(int count) {
+      _logger.LogInformation($"{HttpContext.Request.Method} {HttpContext.Request.GetDisplayUrl()}");
+      var rng = new Random();
+      if (count <= 0) {
+        count = 2;
+      }
+
+      return Enumerable.Range(count, count)
+        .Select(index => new WeatherForecast {
+          Date = DateTime.Now.AddDays(index),
+          TemperatureC = rng.Next(-20, 55),
+          Summary = Summaries[rng.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost]
+    public ModelData Post([FromBody]ModelData model) {
+      _logger.LogInformation($"{HttpContext.Request.Method} {HttpContext.Request.GetDisplayUrl()}");
+      model.Id++;
+      model.Name = $"{model.Name} in business";
+      return model;
+    }
+
+    [HttpDelete]
+    public string Delete([FromBody]ModelData model) {
+      _logger.LogInformation($"{HttpContext.Request.Method} {HttpContext.Request.GetDisplayUrl()}");
+      return$"删除对象【id:{model.Id} name:{model} ";
+    }
+  }
+}
---
+++ GatewayApis/Models/ModelData.cs
@@ -0,0 +1,6 @@
+namespace Models {
+  public class ModelData {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
