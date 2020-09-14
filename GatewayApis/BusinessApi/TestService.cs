8a8abab29613efc8f35b52b782635ff80b33a6ae
mahuwei <mahuwei@msn.com>
2020-09-14 15:09:05 +0800

    测试

--- GatewayApis/BusinessApi/TestService.cs
+++ GatewayApis/BusinessApi/TestService.cs
@@ -9,16 +9,27 @@ using Newtonsoft.Json;
 namespace BusinessApi {
   public class TestService : IHostedService {
     private readonly ILogger<TestService> _logger;
+    private BaseConfig _config;
 
     public TestService(ILogger<TestService> logger, IOptionsMonitor<BaseConfig> options) {
       _logger = logger;
 
       _logger.LogDebug("config setting:{@config}",
         JsonConvert.SerializeObject(options.CurrentValue, Formatting.Indented));
+      _config = options.CurrentValue;
 
       options.OnChange(op => {
         _logger.LogInformation("HostSample Config change:{@newConfig}",
           JsonConvert.SerializeObject(options.CurrentValue, Formatting.Indented));
+
+        if (_config.Kafka != options.CurrentValue.Kafka) {
+          _logger.LogInformation($"Kafka发生变化:{_config.Kafka} ==> {options.CurrentValue.Kafka}");
+        }
+        else {
+          _logger.LogInformation($"Kafka没有发生变化:{_config.Kafka} ==> {options.CurrentValue.Kafka}");
+        }
+
+        _config = options.CurrentValue;
       });
     }
 
